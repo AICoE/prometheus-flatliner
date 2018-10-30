24da096152168fe3651a87929edb1806b54f2dc9
Subhojit Mukherjee <subhojit_mukherjee_1988@yahoo.co.in>
2018-10-30 16:35:19 +0530

    Dataframe creation with alerts

--- flatliners/dfgen.py
+++ flatliners/dfgen.py
@@ -1,5 +1,6 @@
 from .baseflatliner import BaseFlatliner
-
+import pandas as pd
+from datetime import datetime
 
 
 class DfGenerator(BaseFlatliner):
@@ -7,6 +8,10 @@ class DfGenerator(BaseFlatliner):
         print(" Inside alert corr")
         super().__init__()
         self.clusters = dict()
+        ### Empty dataframe placeholder
+        self.df = pd.DataFrame(columns=['_id', 'timestamp', 'alertname'])
+        ### To keep track of previous publish
+        self.timestamp = 0
 
     def on_next(self, x):
         """ update calculate std dev for cluster
@@ -29,15 +34,34 @@ class DfGenerator(BaseFlatliner):
             previous = self.clusters[cluster_id][alert_name]
             previous.extend(self.metric_values(x))
             self.clusters[cluster_id][alert_name] = previous
-        #self.clusters[cluster_id] =
-        self.print_values(self.clusters[cluster_id])
+        previous_df = self.df
 
-        self.publish(self.clusters[cluster_id])
+        ### Dataframe creation
+        timestamp = []
+        cluster = []
+        alert_name_list = []
+
+        for elem in self.metric_values(x):
+            timestamp.append(elem[0])
+            cluster.append(cluster_id)
+            alert_name_list.append(alert_name)
+        self.df = previous_df.append(pd.DataFrame({'_id': cluster, 'timestamp': timestamp, 'alertname': alert_name_list}))
+        self.df.reset_index(drop= True, inplace= True)
+
+
+        ### Publishing for every hour
+        if(timestamp[-1] - self.timestamp) > 3600:
+            self.print_values(self.df, timestamp[-1])
+            self.timestamp = timestamp[-1]
+            self.publish(self.df)
+
 
     def on_completed(self):
         #print(self.clusters)
         print("Done!")
 
+
     @staticmethod
-    def print_values(clusters):
-        return clusters
+    def print_values(dataframe, timestamp):
+        print("Printing for :", datetime.fromtimestamp(timestamp))
+        return dataframe.groupby(by=['_id', 'alertname']).count()
