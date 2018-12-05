73bbd54ea883c8ae27eaf935241c625bdb647344
Anand Sanmukhani <asanmukh@redhat.com>
2018-12-04 23:56:45 -0500

    Remove repeated function from PromMetrics class

--- metrics/prommetrics.py
+++ metrics/prommetrics.py
@@ -34,33 +34,6 @@ class PromMetrics:
                                         start_time=self.metric_start_datetime,
                                         end_time=self.metric_end_datetime,
                                         chunk_size=self.metric_chunk_size)
-        
-    def _get_metrics_from_prometheus(self, observer=None):
-        # Collect credentials to connect to a prometheus instance
-        prom_token = os.getenv("PROM_ACCESS_TOKEN")
-        prom_url = os.getenv("PROM_URL")
-        if not (prom_token or prom_url):
-            sys.exit("Error: Prometheus credentials not found")
-
-
-        prom = Prometheus(url=prom_url, token=prom_token, data_chunk='5m',stored_data='5m')
-
-        metrics_list = prom.all_metrics() # Get a list of all the metrics available from Prometheus
-
-        print("Polling Prometheus for new metric data")
-
-        metric_data = dict()
-        if observer:
-            for metric in metrics_list:
-                pkt = (json.loads(prom.get_metric(name=metric))[0])
-                metric_data[metric] = pkt
-                observer.on_next(pkt) # push metric data to the Observer
-            pass
-        else:
-            for metric in metrics_list:
-                metric_data[metric] = (json.loads(prom.get_metric(name=metric))[0])
-
-        return(metric_data)
 
     def observe_prom_metrics_range(self, observer, metrics_list, start_time, end_time='now', chunk_size='1h'):
 
@@ -74,7 +47,6 @@ class PromMetrics:
         prom = Prometheus(url=prom_url, token=prom_token)
 
         chunk_seconds = int(round((dateparser.parse('now') - dateparser.parse(chunk_size)).total_seconds()))
-
 
         start = dateparser.parse(start_time).timestamp()
         end = dateparser.parse(end_time).timestamp()
