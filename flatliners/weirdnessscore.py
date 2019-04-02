ef9519bd719d828719a3058deeabbf7c1db6a931
Anand Sanmukhani <asanmukh@redhat.com>
2019-04-02 14:31:32 -0400

    Fix Potential bloats in WeirdnessScore Flatliner

--- flatliners/weirdnessscore.py
+++ flatliners/weirdnessscore.py
@@ -6,9 +6,31 @@ from dataclasses import dataclass
 class WeirdnessScore(BaseFlatliner):
     def __init__(self):
         super().__init__()
+        # this dict needs to store the resource state for each cluster
+        # promql: count (count (etcd_object_counts) by (_id))
+        # example:
+        # 4f3fa2db-bcc7-4a70-bc53-2501ebbf235a: {cluster='4f3fa2db-bcc7-4a70-bc53-2501ebbf235a',
+        #                                        version='4.0.0-0.7',
+        #                                        resource='roles.rbac.authorization.k8s.io',
+        #                                        std_dev=0.12843759382881617,
+        #                                        timestamp=1554219689.252,
+        #                                        weirdness_score=0.12750775288668642,
+        #                                        std_dev_buffer=True,
+        #                                        resource_deltas={'alertmanagers.monitoring.coreos.com': 0.001620557938884463,
+        #                                                         'apiservices.apiregistration.k8s.io': 0.03206530312721848
+        #                                                         ..................}
+        self.resource_score = self.create_cache_dict(maxsize=1000)
 
-        self.resource_score = dict()
-        self.alert_score = dict()
+        # this dict needs to store the alert state for each cluster
+        # promql: count (count (etcd_object_counts) by (_id))
+        # fd6383f1-414c-4361-8a3e-e7eaed578eed {cluster='fd6383f1-414c-4361-8a3e-e7eaed578eed',
+        #                                       version='4.0.0-0.7',
+        #                                       alert='KubeClientCertificateExpiration',
+        #                                       weirdness_score=0.0853890113837755,
+        #                                       timestamp=1554219956.683,
+        #                                       alert_deltas={'KubeClientCertificateExpiration': 0.008346244408562575,
+        #                                                     'Watchdog': 0.08498013573401186})
+        self.alert_score = self.create_cache_dict(maxsize=1000)
 
     def on_next(self, x):
 
